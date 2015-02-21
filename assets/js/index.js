a355f48b83e21f1f27d96bffe46f2d9719b7526b
Thiago Belem <contato@thiagobelem.net>
2015-02-21 11:39:25 -0200

    Dont create caption for emojis

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@
     });
 
     // Creates Captions from Alt tags
-    $(".post-content img").each(function() {
+    $(".post-content img:not(.emoji)").each(function() {
       // Let's put a caption if there is one
       if ($(this).attr("alt"))
         $(this).wrap('<figure class="image"></figure>')
